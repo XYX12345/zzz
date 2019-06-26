033d25d2fad656770bf52897e060d6bfcfc036d1
XYX12345 <52227863+XYX12345@users.noreply.github.com>
2019-06-26 16:43:39 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,79 @@
+var json = {
+  "data": [
+    {
+      'id': 0,
+      "longitude":111.2,
+      "latitude": 36.97,
+      "placeName":"吕梁玫瑰",
+      "news":"吕梁带刺的玫瑰",
+      'title': '一往情深',
+      "imgUrl": "https://cdn.huaduocai.net//Storage/Shop/3/Products/6964/419754588884365.jpg",
+      "type": [
+        {
+          "imgUrl": "https://cdn.huaduocai.net//Storage/Shop/3/Products/6964/419754588884365.jpg",
+          "imgUrl2": "https://cdn.huaduocai.net//Storage/Shop/3/Products/6964/Details/10868606378708290.jpg",
+          "imgUrl3": "https://cdn.huaduocai.net//Storage/Shop/3/Products/6964/Details/10868632902889354.jpg",
+          
+          "name": "一往情深",
+          "price": "319.99",
+          "oldprice": "519.99",
+          "detailmess": "	你的轻柔像阵微风，让我从容不迫，想让你知道，我对你始终一往情深。",
+          "message": '33支红玫瑰，粉色桔梗外围一圈，高级雾面纸包扎，丝带蝴蝶结',
+          "renqun":"恋人、长辈、朋友、客户、领导"
+        },
+      ],
+      
+    },
+    {
+      'id': 1,
+      "longitude":113.62,
+      "latitude": 37.8,
+      "placeName": "阳泉玫瑰",
+      "news": "阳泉带刺的玫瑰",
+      'title': '花开漫恋',
+      "imgUrl": "https://cdn.huaduocai.net//Storage/Shop/3/product/5434/1.jpg",
+      "type": [
+        {
+          "imgUrl": "https://cdn.huaduocai.net//Storage/Shop/3/product/5434/1.jpg",
+          "imgUrl2": "https://cdn.huaduocai.net//Storage/Shop/3/product/5434/details/15561596242315434855.jpg",
+          "imgUrl3": "https://cdn.huaduocai.net//Storage/Shop/3/product/5473/details/15561596331235473360.jpg",
+          "name": "花开漫恋",
+          "price": "639.99",
+          "oldprice": "699.99",
+          "detailmess": "	给我一个承诺，我哪里都不会去，就站在这里等着你。",
+          "message": '99朵白玫瑰，勿忘我搭配。',
+          "renqun": "恋人、长辈、朋友、客户、领导"
+        },
+      ],
+     
+    },
+    {
+      'id': 2,
+      "longitude": 112.48,
+      "latitude": 37.93,
+      "placeName": "尖草坪玫瑰",
+      "news": "尖草坪带刺的玫瑰",
+      'title': '此情可待',
+      "imgUrl": "https://cdn.huaduocai.net/Storage/Shop/3/product/6554/1.jpg",
+      "type": [
+        {
+          "imgUrl": "https://cdn.huaduocai.net/Storage/Shop/3/product/6554/1.jpg",
+          
+          "imgUrl2": "https://cdn.huaduocai.net//Storage/Shop/3/product/5147/details/15561595522825147163.jpg",
+          "imgUrl3": "https://cdn.huaduocai.net//Storage/Shop/3/product/4673/details/15561594732794673417.jpg",
+          "name": "此情可待",
+          "price": "889.99",
+          "oldprice": "999.99",
+          "detailmess": "	恋一个善良的你，说一席永不离弃的话，走一生风雨携手的路，护一颗纯真柔弱的心，守一段天长地久的情，开一朵天荒地老的花",
+          "message": '	精选19朵进口七彩玫瑰，优质绿叶满天星围绕',
+
+          "renqun": "恋人、长辈、朋友、客户、领导"
+        },
+      ],
+      
+    },
+  ]
+}
+module.exports = {
+  data: json
+}
